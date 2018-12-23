1d6a4599a8e88b98fe4f4931bc6f97c04649a1cf
Rob Lundie Hill <rob.lundiehill@thomsonreuters.com>
2018-12-23 15:15:51 +0000

    installer fix

--- install_on_pi.sh
+++ install_on_pi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 location=$(pwd)
-cat startup-script | sed 's/__LOCATION__/'$location > /etc/init.d/one-second-dash
+cat startup-script | sed 's|__LOCATION__|'$location'|' > /etc/init.d/one-second-dash
 chmod 755 /etc/init.d/one-second-dash
 update-rc.d one-second-dash defaults
