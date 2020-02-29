3a68867d4da7fc29c2354dcf7f0addef1c03b5c8
Rob Lundie Hill <rob@lundiehill.com>
2020-02-29 20:10:21 +0000

    change filenames to include location symlinks

--- launch-playlist.sh
+++ launch-playlist.sh
@@ -9,25 +9,35 @@ curl https://maker.ifttt.com/trigger/dash-$1/with/key/$IFTTT_MAKER_KEY
 
 
 if [ "$1" == Sweepy-Gripey ]; then
+
     player=bedroom
     playlist=spotify:user:rangelife2:playlist:0SMpzciQeLySoYwkmhCDHd
     volume=60
     shuffle=1
+
 elif [ "$1" == Sweepy-Poo-Poo ]; then
+
     player=spareroom
     playlist=spotify:playlist:6tE5ODrv37riZ2QwsFiJuL
     volume=80
     shuffle=0
+
 elif [ "$1" == Sweepy-Puff-Buff ]; then
+
+    curl http://localhost:3888/scene/Kitchen
+
     player=kitchen
     playlist=spotify:playlist:3Cuz5dYY5q0z9rzQxWxrB4
     volume=80
     shuffle=1
+
 elif [ "$1" == Sweepy-Scrub-Scrub ]; then
+
     player=livingroom
     playlist=spotify:playlist:4sEDNtIxg3bryGwP24ZoQj
     volume=80
     shuffle=1
+
 else
     echo "UNRECOGNISED BUTTON"
     exit 1
