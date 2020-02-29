619dad27a5f62bcc9b4822f28727a726f48000c7
Rob Lundie Hill <rob@lundiehill.com>
2020-02-29 09:52:15 +0000

    added 3 more dash buttons

--- launch-playlist.sh
+++ launch-playlist.sh
@@ -12,10 +12,22 @@ if [ "$1" == Sweepy-Gripey ]; then
     player=bedroom
     playlist=spotify:user:rangelife2:playlist:0SMpzciQeLySoYwkmhCDHd
     volume=60
+    shuffle=1
 elif [ "$1" == Sweepy-Poo-Poo ]; then
     player=spareroom
     playlist=spotify:playlist:6tE5ODrv37riZ2QwsFiJuL
     volume=80
+    shuffle=0
+elif [ "$1" == Sweepy-Puff-Buff ]; then
+    player=kitchen
+    playlist=spotify:playlist:3Cuz5dYY5q0z9rzQxWxrB4
+    volume=80
+    shuffle=1
+elif [ "$1" == Sweepy-Scrub-Scrub ]; then
+    player=livingroom
+    playlist=spotify:playlist:4sEDNtIxg3bryGwP24ZoQj
+    volume=80
+    shuffle=1
 else
     echo "UNRECOGNISED BUTTON"
     exit 1
@@ -23,9 +35,13 @@ fi
 
 
 
-# bedroom playlist
-squeezeplay $player --raw playlist shuffle 1 &
+#squeezeplay $player --raw playlist shuffle 1 &
+#squeezeplay $player --raw mixer volume 0
+
+squeezeplay $player --raw sync -
 squeezeplay $player --raw mixer volume 0
+squeezeplay $player --raw playlist shuffle $shuffle
+
 
 # xmas
 # curl http://localhost:3888/scene/xmas
