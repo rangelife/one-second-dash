97fc062c214de20ae63819ddde7e24da8fdb4df6
Rob Lundie Hill <rob@lundiehill.com>
2020-06-18 23:28:25 +0100

    dining button turns off tv

--- launch-lights-dining.sh
+++ launch-lights-dining.sh
@@ -2,3 +2,8 @@
 
 echo setting dining lights mode
 curl http://localhost:3888/scene/Dinner
+
+curl 'http://living-room-tv/sony/system' -H 'Connection: keep-alive' -H 'X-Auth-PSK: 2019' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS Xt/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: */*' -H 'Origin: null' -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' --data-binary '{"method":"setPowerStatus","version":"1.0","id":1,"params":[{"status":false}]}' --compressed --insecure
+
+
+
